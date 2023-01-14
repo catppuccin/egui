16e1d5ae2e7bf86b0ea21b5e0c52e6eebfabc409
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-01-14 17:17:08 -0500

    fix badges

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! # Example
 //!
-//! ```no_test,rust
+//! ```ignore,rust
 //! struct App;
 //! impl eframe::App for App {
 //!     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
