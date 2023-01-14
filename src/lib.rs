e18dc3a297195d87abcad325e99ad8dde1620701
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-01-14 17:11:48 -0500

    disable doctest

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! # Example
 //!
-//! ```rust,no_test
+//! ```no_test,rust
 //! struct App;
 //! impl eframe::App for App {
 //!     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
