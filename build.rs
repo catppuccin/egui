73e0d2e21a22a07718681ee2d58b1e4688b8b35d
Sam Nystrom <sam@samnystrom.dev>
2024-04-22 19:55:38 -0400

    fix: don't require whiskers in build.rs

--- build.rs
+++ build.rs
@@ -1,16 +1,32 @@
-use std::{fs::OpenOptions, process::Command};
+use std::{fs, path::Path, process::{Command, ExitCode}};
 
-fn main() {
+fn main() -> ExitCode {
     println!("cargo::rerun-if-changed=src/themes.rs.tera");
-    let fh = OpenOptions::new()
+
+    // src/themes.rs.tera is excluded from the distributed crate so whiskers is
+    // not run if the crate is used as a dependency.
+    if !Path::new("src/themes.rs.tera").exists() {
+        return ExitCode::SUCCESS;
+    }
+
+    let outfile = fs::OpenOptions::new()
         .write(true)
         .create(true)
         .open("src/themes.rs")
         .unwrap();
-    let stat = Command::new("whiskers")
+
+    let Ok(stat) = Command::new("whiskers")
         .arg("src/themes.rs.tera")
-        .stdout(fh)
+        .stdout(outfile)
         .status()
-        .unwrap();
-    assert!(stat.success());
+    else {
+        println!("cargo::warning=Failed to run whiskers (https://github.com/catppuccin/toolbox/tree/main/whiskers). Is it installed?");
+        return ExitCode::SUCCESS;
+    };
+
+    if !stat.success() {
+        println!("cargo::warning=whiskers exited nonzero");
+        return ExitCode::FAILURE;
+    }
+    return ExitCode::SUCCESS;
 }
