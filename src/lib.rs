316466130544cea2026fb795ee01b6a6bd1c266d
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2024-07-26 05:02:24 +0200

    bump to 28

--- src/lib.rs
+++ src/lib.rs
@@ -30,14 +30,6 @@
 //! ```
 //!
 
-#[cfg(not(any(feature = "egui26", feature = "egui27")))]
-compile_error!("at least one egui version must be enabled");
-
-#[cfg(feature = "egui26")]
-use egui26 as egui;
-#[cfg(feature = "egui27")]
-use egui27 as egui;
-
 use egui::{epaint, style};
 
 mod themes;
